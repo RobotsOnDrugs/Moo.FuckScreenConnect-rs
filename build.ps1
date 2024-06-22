0cf1a1908f6431418e3650655ff2a8f1ce9934cf
RobotsOnDrugs <robotsondrugs@gmail.com>
2024-06-22 12:48:30 -0500

    Added build scripts.

---
+++ build.ps1
@@ -0,0 +1,48 @@
+
+<#
+  .DESCRIPTION
+  Builds the FSC executable and prepares it for release. Final binaries will be copied to .\build and compressed to 7zip (LZMA) and zip archives.
+  .PARAMETER FileDescription <Description>
+  Optional; no metadata is set if unset. Specifies the value of FileDescription in the binary's manifest, which is what will displayed in Task Manager. Specifying this with something innocuous this is recommended in case a very suspicious scammer looks at Task Manager.
+  .PARAMETER AdditionalOutputDirs
+  Optional. An array of additional directories to copy binaries to. Must be absolute paths. Files copied to additional directories will not be packed into archives.
+#>
+
+param
+(
+	[Parameter()][string]$FileDescription = '',
+	[Parameter()][System.Collections.Generic.HashSet[String]]$AdditionalOutputDirs
+)
+$name = 'Moo.FuckScreenConnect-rs'
+$version = (cargo.exe read-manifest --manifest-path .\Cargo.toml | ConvertFrom-Json).version
+$ErrorActionPreference = 'Stop'
+
+$env:BINARY_FILE_DESCRIPTION=$FileDescription
+
+if ($Clean) { cargo.exe clean }
+if ($LASTEXITCODE -eq 0)
+{
+	$new_path = '.\build'
+	$(Remove-Item -Recurse -Force -Path $new_path -ErrorAction SilentlyContinue) | Out-Null
+	$(New-Item -Force -Type Directory $new_path) | Out-Null
+	cargo.exe build
+	cargo.exe build --release
+	Copy-Item -Path '.\target\debug\moo_fuck_screen_connect.exe' -Destination '.\build\moo_fuck_screen_connect_debug.exe'
+	Copy-Item -Path '.\target\release\moo_fuck_screen_connect.exe' -Destination '.\build\moo_fuck_screen_connect.exe'
+	Copy-Item -Path '.\install.ps1' -Destination '.\build\install.ps1'
+	Copy-Item -Recurse -Path '.\install_modules' -Destination '.\build\'
+}
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+Push-Location -Path .\build
+foreach ($additional_output_dir in $AdditionalOutputDirs)
+{
+	if (![System.IO.Path]::IsPathRooted($additional_output_dir)) { Write-Error "'$($additional_output_dir)' is not an absolute path!" }
+	if (!(Test-Path $additional_output_dir)) { Out-Null $(New-Item -Type Directory $additional_output_dir) }
+	$(Remove-Item -Recurse -Path $additional_output_dir -ErrorAction SilentlyContinue) | Out-Null
+	Copy-Item -Recurse '.' $additional_output_dir
+}
+$(Remove-Item -Force -Path test.7z -ErrorAction SilentlyContinue) | Out-Null
+7za a -t7z -m0=LZMA2 -mx9 -mmt8 -aoa "${name}.${version}.7z" * | Out-Null
+7za a -tzip -m0=Deflate64 -mpass=15 -mfb=256 -mx9 -mmt8 -aoa "${name}.${version}.zip" * | Out-Null
+Pop-Location
---
+++ build.rs
@@ -0,0 +1,25 @@
+use embed_manifest::embed_manifest;
+use embed_manifest::new_manifest;
+use embed_manifest::manifest::ExecutionLevel;
+
+use winresource::WindowsResource;
+
+fn main()
+{
+	let manifest_builder = new_manifest("default")
+		.requested_execution_level(ExecutionLevel::RequireAdministrator);
+	embed_manifest(manifest_builder).expect("Couldn't embed manifest.");
+
+	let mut res = WindowsResource::new();
+	let file_description = std::env::var("BINARY_FILE_DESCRIPTION");
+	if let Ok(desc) = file_description
+	{
+		res.set("FileDescription", desc.as_str());
+		res.set("ProductName", desc.as_str());
+	};
+	match res.compile()
+	{
+		Ok(()) => {}
+		Err(err) => { eprintln!("Something went wrong creating metadata for the executable! {:?}", err); panic!() }
+	};
+}
