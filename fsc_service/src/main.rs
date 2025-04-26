dc94c60aee1e073b614485ac97f5e59c06378909
RobotsOnDrugs <robotsondrugs@gmail.com>
2025-04-26 14:20:53 -0500

    Intermediate commit for the move to a service model.
    The install script has been completely revamped to install the service instead of creating a scheduled task and all the code to get PsExec has been removed.
    For the time being, the name of the service will have to be passed as a command line argument to the service. This is reflected in the install script.
    The path to the core is now assumed to be 'fsc_core.exe' in the same directory as the service. This may change in the future.

--- fsc_service/src/main.rs
+++ fsc_service/src/main.rs
@@ -4,7 +4,10 @@
 
 #![cfg_attr(debug_assertions, allow(unused_imports))]
 
+use std::env::args;
 use std::ffi::OsString;
+use std::os::windows::ffi::OsStrExt;
+use std::path::Path;
 use std::process::exit;
 use std::ptr::null_mut;
 use std::thread::sleep;
@@ -18,6 +21,7 @@ use simplelog::WriteLogger;
 
 use windows::core::w;
 use windows::core::PWSTR;
+use windows::Win32::Foundation::CloseHandle;
 use windows::Win32::Foundation::GetLastError;
 use windows::Win32::Foundation::HANDLE;
 use windows::Win32::Security::DuplicateTokenEx;
@@ -27,6 +31,7 @@ use windows::Win32::Security::TOKEN_ACCESS_MASK;
 use windows::Win32::Security::TOKEN_ALL_ACCESS;
 use windows::Win32::System::Environment::CreateEnvironmentBlock;
 use windows::Win32::System::Threading::CreateProcessAsUserW;
+use windows::Win32::System::Threading::PROCESS_TERMINATE;
 use windows::Win32::System::Threading::GetCurrentProcess;
 use windows::Win32::System::Threading::GetCurrentProcessId;
 use windows::Win32::System::Threading::OpenProcess;
@@ -53,9 +58,7 @@ use windows_service::service_control_handler::ServiceStatusHandle;
 use fsc_common::logging::get_default_config;
 use fsc_common::logging::get_default_file;
 
-#[cfg_attr(debug_assertions, allow(dead_code))]
-const DEFAULT_SERVICE_NAME: &str = "fscserv";
-const PROCESS_NAME: &str = r#""#;
+const FSC_CORE_NAME: &str = "fsc_core.exe"; 
 
 define_windows_service!(ffi_service_main, service_entry);
 
@@ -64,31 +67,42 @@ fn main() -> Result<(), windows_service::Error>
 	WriteLogger::init(LevelFilter::Info, get_default_config(), get_default_file()).unwrap();
 
 	info!("Starting main.");
-	info!("{:?}", std::env::args());
+	let service_name = match args().nth(1)
+	{
+		None => { error!("No service name was supplied as an argument. Cannot continue."); return Err(windows_service::Error::LaunchArgumentsNotSupported); },
+		Some(service_name) => service_name
+	};
+	info!("service_name: {service_name}");
 	#[cfg_attr(debug_assertions, cfg(any()))]
-	windows_service::service_dispatcher::start(DEFAULT_SERVICE_NAME, ffi_service_main)?;
+	windows_service::service_dispatcher::start(service_name, ffi_service_main)?;
 	#[cfg(debug_assertions)]
 	service_entry(Vec::new());
 	return Ok(());
 }
 
-fn service_entry(_: Vec<OsString>)
+#[cfg_attr(debug_assertions, allow(dead_code))]
+fn service_entry(args: Vec<OsString>)
 {
 	info!("Starting service.");
+	
+	// Currently, the only way to figure out the service name for the status handle is to have it as a command line argument
+	// Maybe it'll enumerate services and properly find itself one day
+	#[cfg_attr(debug_assertions, allow(unused_variables))]
+	let service_name = &args[0];
 
 	#[cfg_attr(debug_assertions, cfg(any()))]
 	let status_handle: ServiceStatusHandle;
 	#[cfg_attr(debug_assertions, cfg(any()))]
 	{
 		let event_handler = move |control_event| -> ServiceControlHandlerResult
+		{
+			return match control_event
 			{
-				return match control_event
-				{
-					ServiceControl::Stop | ServiceControl::Interrogate => { ServiceControlHandlerResult::NoError }
-					_ => ServiceControlHandlerResult::NoError,
-				}
-			};
-		status_handle = service_control_handler::register(DEFAULT_SERVICE_NAME, event_handler).unwrap();
+				ServiceControl::Stop | ServiceControl::Interrogate => { ServiceControlHandlerResult::NoError }
+				_ => ServiceControlHandlerResult::NoError,
+			}
+		};
+		status_handle = service_control_handler::register(service_name, event_handler).unwrap();
 		let next_status = ServiceStatus
 		{
 			// Should match the one from system service registry
@@ -130,7 +144,6 @@ fn service_entry(_: Vec<OsString>)
 			TerminateProcess(GetCurrentProcess(), 1).unwrap();
 		}
 
-		// let creation_flags = CREATE_UNICODE_ENVIRONMENT | CREATE_NEW_CONSOLE;
 		let creation_flags = CREATE_UNICODE_ENVIRONMENT | CREATE_NO_WINDOW;
 		let startup_info = STARTUPINFOW
 		{
@@ -138,32 +151,53 @@ fn service_entry(_: Vec<OsString>)
 			wShowWindow: SW_HIDE.0 as _,
 			..Default::default()
 		};
-		let mut buf= PROCESS_NAME.encode_utf16().collect::<Vec<u16>>();
+		let current_exe_path = std::env::current_exe().unwrap();
+		let current_dir = Path::new(current_exe_path.as_os_str()).parent().unwrap();
+		let core_path = current_dir.join(Path::new(FSC_CORE_NAME));
+		info!("{:?}", core_path);
+		let mut buf = core_path.as_os_str().encode_wide().collect::<Vec<u16>>();
 		buf.push(0);
 		let app_path = PWSTR::from_raw(buf.as_mut_ptr());
+		
 		let mut process_info = PROCESS_INFORMATION::default();
 		let create_process = CreateProcessAsUserW(Some(duplicated_token_handle), app_path, None, None, None, false, creation_flags, Some(env), PWSTR::null(), &startup_info, &mut process_info);
 		if create_process.is_err()
 		{
-			error!("Error creating process: {:x}", GetLastError().0);
+			error!("Error creating FSC core process: {:x}", GetLastError().0);
+			info!("Stopping service.");
+			#[cfg_attr(debug_assertions, cfg(any()))]
+			update_service_status(&status_handle, ServiceState::Stopped, ServiceControlAccept::empty(), 0).unwrap();
 			return;
 		}
+		info!("Started core with pid {}.", process_info.dwProcessId);
+		
+		// Pretend to do something for a little bit and then terminate core and exit for demonstration purposes.
+		// Also, the service can't respond to stop messages yet.
+		sleep(Duration::from_secs(5));
+		let child_proc = OpenProcess(PROCESS_TERMINATE, false, process_info.dwProcessId).unwrap();
+		TerminateProcess(child_proc, 0).unwrap();
+		let _ = CloseHandle(child_proc);
 	}
 	
 	info!("Stopping service.");
-	// Tell the system that service has stopped.
 	#[cfg_attr(debug_assertions, cfg(any()))]
-	status_handle.set_service_status
+	update_service_status(&status_handle, ServiceState::Stopped, ServiceControlAccept::empty(), 0).unwrap();
+}
+
+#[cfg_attr(debug_assertions, allow(dead_code))]
+fn update_service_status(service_status_handle: &ServiceStatusHandle, service_state: ServiceState, controls: ServiceControlAccept, exit_code: u32) -> Result<(), windows_service::Error>
+{
+	return service_status_handle.set_service_status
 	(
 		ServiceStatus
 		{
 			service_type: ServiceType::OWN_PROCESS,
-			current_state: ServiceState::Stopped,
-			controls_accepted: ServiceControlAccept::empty(),
-			exit_code: ServiceExitCode::Win32(0),
+			current_state: service_state,
+			controls_accepted: controls,
+			exit_code: ServiceExitCode::Win32(exit_code),
 			checkpoint: 0,
 			wait_hint: Duration::default(),
 			process_id: None,
 		}
-	).unwrap();
+	)
 }
--- install_modules/fsc_functions.ps1
+++ install_modules/fsc_functions.ps1
@@ -1,245 +1,48 @@
-$ScheduledTaskName = 'FSC Service Task'
-$InstallationKeyPath = 'HKLM:\SOFTWARE\Moo\FuckScreenConnect'
-$FileNames = @('moo_fuck_screen_connect.exe', 'moo_fuck_screen_connect_debug.exe')
-
-. .\install_modules\external_tools.ps1
-
 enum ExistingInstallationStatus
 {
 	None
 	Normal
-	Update
 	Broken
 }
 
-function Get-Installation
-{
-	$keypath = $null
-	try { $keypath = $(Get-Item -Path $InstallationKeyPath) }
-	catch [System.Management.Automation.ItemNotFoundException] { Write-Log -Level DEBUG -Message "Installation configuration is not set."; return $null }
-	catch
-	{
-		Stop-ScriptWithError -ErrorMessage "There was a non-recoverable error getting installation information: $($_.Exception.Message)"
-	}
-	if ($keypath)
-	{
-		[Microsoft.Win32.RegistryKey]$keypath = $keypath
-		Write-Log -Level DEBUG -Message "Got keypath."
-		return $keypath
-	}
-	else
-	{
-		Stop-ScriptWithError -ErrorMessage "State for Get-Installation is likely invalid."
-	}
-}
-$InstallationKey = Get-Installation
-function Set-Installation
-{
-	param([string]$InstallationSource, [string]$install_path, [bool]$files_only)
-	Write-Log -Level DEBUG -Message "Setting installation."
-	New-Installation $InstallationSource $install_path $files_only
-	Write-Log -Level DEBUG -Message "Skipping installation."
-}
-function New-Installation
-{
-	param([String]$InstallationSource, [String]$install_path, [bool]$files_only)
-
-	if (-not $files_only)
-	{
-		Write-Log -Level INFO -Message "Creating a new installation."
-		Remove-Item -Path $InstallationKeyPath -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
-	}
-
-	Remove-Item $install_path -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
-	New-Item -Type Directory $install_path -Force -Confirm:$false | Out-Null
-	Get-PsExec | Out-Null
-	Move-Item 'PsExec.exe' $install_path
-	$psexec_path = Join-Path $install_path 'PsExec.exe'
-	foreach ($file_name in $FileNames)
-	{
-		try { Copy-Item $file_name $install_path }
-		catch [System.Management.Automation.ItemNotFoundException]
-		{
-			Remove-Item -Recurse $install_path -Force -ErrorAction SilentlyContinue | Out-Null
-			Stop-ScriptWithError -ErrorMessage "$file_name was not found in the current directory. Cannot continue."
-		}
-		catch
-		{
-			Remove-Item -Recurse $install_path -Force -ErrorAction SilentlyContinue | Out-Null
-			Stop-ScriptWithError -ErrorMessage "Could not copy files: $($Error[0].Message). Cannot continue."
-		}
-	}
-	Write-Log -Level INFO -Message "Files copied to $install_path."
-
-	$key = New-Item -Path $InstallationKeyPath -Force
-	$key.SetValue('InstallationPath', $install_path)
-	$log_dir = New-Item -Type Directory $(Join-Path $install_path 'logs')
-	$key.SetValue('LogDirectory', $log_dir)
-	$key.SetValue('PsExecPath', $psexec_path)
-	Write-Log -Level INFO -Message "Configuration created."
-	if ($files_only)
-	{
-		Write-Log -Level INFO -Message "Retaining old scheduled task and configuration."
-		return
-	}
-
-	$full_path = Join-Path $install_path 'moo_fuck_screen_connect.exe'
-	$task_action = New-ScheduledTaskAction -Execute $psexec_path -Argument "-s -i 1 -w `"$install_path`" `"$full_path`"" -WorkingDirectory "$install_path"
-	$task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -Compatibility Win8 -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -Hidden -MultipleInstances IgnoreNew -RestartCount 3 -StartWhenAvailable -WakeToRun
-	$task_trigger = New-ScheduledTaskTrigger -AtLogon
-	$task_principal = New-ScheduledTaskPrincipal -LogonType S4U -RunLevel Highest -UserId (whoami)
-	Register-ScheduledTask -TaskName $ScheduledTaskName -Action $task_action -Principal $task_principal -Settings $task_settings -Trigger $task_trigger | Out-Null
-	Write-Log -Level INFO -Message "Scheduled task created."
-}
-
-function Get-FileInstallationStatus
-{
-	param([String]$install_path, [bool]$install)
-
-	if (-not $(Get-Installation)) { return [ExistingInstallationStatus]::None }
-	$expected_path_properties = @('InstallationPath', 'LogDirectory')
-	foreach ($property in $expected_path_properties)
-	{
-		$value = $InstallationKey.GetValue($property)
-		if ($null -eq $value)
-		{
-			Write-Log -Level WARNING -Message "Value for $property is missing."
-			return [ExistingInstallationStatus]::Broken
-		}
-		if ($null -eq $(Get-Item $value -ErrorAction SilentlyContinue)) {return [ExistingInstallationStatus]::None }
-	}
-
-	if (-not $install_path) { $install_path = $InstallationKey.GetValue('InstallationPath') }
-	try { Get-Item $install_path -ErrorAction Stop }
-	catch
-	{
-		if (-not $install_path) { Write-Log -Level DEBUG -Message "No installation path was specified." }
-		else { Write-Log -Level DEBUG -Message "$install_path missing." }
-		return [ExistingInstallationStatus]::None
-	}
-	return [ExistingInstallationStatus]::Normal
-}
-function Get-ScheduledTaskInstallationStatus
-{
-	try { $null = Get-ScheduledTask -TaskName $ScheduledTaskName -ErrorAction Stop }
-	catch [Microsoft.PowerShell.Cmdletization.Cim.CimJobException]
-	{
-		if ($_.CategoryInfo.Category -eq [System.Management.Automation.ErrorCategory]::ObjectNotFound) { return [ExistingInstallationStatus]::None }
-		else
-		{
-			Write-Log -Level ERROR -Message "Could not access the FSC scheduled task: $($_.CategoryInfo)"
-			return [ExistingInstallationStatus]::Broken
-		}
-	}
-	catch
-	{
-		Write-Log -Level ERROR -Message "Could not access the FSC scheduled task: $($_)"
-		return [ExistingInstallationStatus]::Broken
-	}
-	return [ExistingInstallationStatus]::Normal
-}
 function Get-InstallationStatus
 {
-	param([String]$install_path, [bool]$install)
-	$files = Get-FileInstallationStatus $install_path $install
-	if ($files -eq [ExistingInstallationStatus]::Broken)
-	{
-		Write-Log -Level DEBUG -Message "Files broken."
-		return [ExistingInstallationStatus]::Broken
-	}
-	$task = Get-ScheduledTaskInstallationStatus
-	if ($task -eq [ExistingInstallationStatus]::Broken) { Write-Log -Level DEBUG -Message "Current scheduled task is broken."; return [ExistingInstallationStatus]::Broken }
-	if ($task -eq [ExistingInstallationStatus]::Normal) { Write-Log -Level DEBUG -Message "Current scheduled task is normal." }
-	if ($task -eq [ExistingInstallationStatus]::None) { Write-Log -Level DEBUG -Message "No scheduled task is currently registered." }
-
-	if ($files -eq [ExistingInstallationStatus]::Normal) { Write-Log -Level DEBUG -Message "Current file installation is normal." }
-	if ($files -eq [ExistingInstallationStatus]::None) { Write-Log -Level DEBUG -Message "Files are not currently installed." }
-	if (($files -eq [ExistingInstallationStatus]::Normal) -and ($task -eq [ExistingInstallationStatus]::Normal)) { return [ExistingInstallationStatus]::Normal }
-	if (($files -eq [ExistingInstallationStatus]::None) -and ($task -eq [ExistingInstallationStatus]::None)) { return [ExistingInstallationStatus]::None }
-	if (($files -eq [ExistingInstallationStatus]::None) -and ($task -eq [ExistingInstallationStatus]::Normal)) { return [ExistingInstallationStatus]::Update }
-	return [ExistingInstallationStatus]::Broken
+	param([string]$service_name)
+	Write-Log -Level DEBUG -Message "Getting installation status for $service_name."
+	$service = Get-WmiObject win32_service | Where-Object {$_.Name -eq $service_name}
+	if ($null -eq $service) { Write-Log -Level DEBUG -Message "Installation status of ${service_name}: None"; return [ExistingInstallationStatus]::None }
+	Write-Log -Level DEBUG -Message ("$($service.PathName)" -replace " $service_name$")
+	if (!(Test-Path -Path ($($service.PathName) -Replace " $service_name$"))) { Write-Log -Level DEBUG -Message "Installation status of ${service_name}: Broken"; return [ExistingInstallationStatus]::Broken }
+	Write-Log -Level DEBUG -Message "Installation status of ${service_name}: Normal"
+	return [ExistingInstallationStatus]::Normal
 }
 
-function Install-Fsc
+function Set-Installation
 {
-	param([String]$install_source, [String]$install_path, [Switch]$force)
-	$status = Get-InstallationStatus $install_path $true
-	if ($status -eq [ExistingInstallationStatus]::Broken)
-	{
-		Stop-ScriptWithError -Message "The existing installation is broken. Cannot continue. Please try forcibly uninstalling FSC or resolving any issues manually."
-	}
-	if ($install_path)
-	{
-		Write-Log -Level INFO -Message "Installation location: ${install_path}."
-		switch ($status)
-		{
-			([ExistingInstallationStatus]::Normal)
-			{
-				Write-Log -Level INFO -Message "FSC is already installed."
-				if ($Force) { Set-Installation $install_source $install_path $false }
-				return
-			}
-			([ExistingInstallationStatus]::None)
-			{
-				Write-Log -Level INFO -Message "Installing from scratch."
-				Set-Installation $install_source $install_path $false
-				break
-			}
-			([ExistingInstallationStatus]::Update)
-			{
-				Write-Log -Level INFO -Message "Updating files."
-				Set-Installation $install_source $install_path $true
-				break
-			}
-			default { "Installation is broken." }
-		}
-	}
-	else
-	{
-		switch ($status)
-		{
-			([ExistingInstallationStatus]::None) { Stop-ScriptWithError -ErrorMessage "No existing installation was found and no installation path was given. Cannot continue."; break }
-			{ (([ExistingInstallationStatus]::Normal) -or ([ExistingInstallationStatus]::Update)) }
-			{
-				$install_path = $InstallationKey.GetValue('InstallationPath')
-				Install-Fsc $install_source $install_path $force
-				break
-			}
-			([ExistingInstallationStatus]::Broken) { Stop-ScriptWithError -ErrorMessage "Installation is broken."; break }
-			default { Stop-ScriptWithError -ErrorMessage "Invalid state when getting installation status. This is a bug."; break }
-		}
-	}
+	param([string]$service_name, [bool]$is_automatic, [string]$path)
+	$not_installed = ((Get-InstallationStatus "$service_name") -eq [ExistingInstallationStatus]::None)
+	if (!$not_installed) { Remove-Installation "$service_name" }
+	Write-Log -Level INFO -Message "Installing $service_name."
+	if ($is_automatic) { $start_type = 'Automatic' }
+	else { $start_type = 'Manual' }
+	New-Service -Name "$service_name" -BinaryPathName "$path $service_name" -DisplayName "$service_name" -StartupType $start_type | Out-Null
+	$status = (Get-InstallationStatus $service_name)
+	if ($status -ne [ExistingInstallationStatus]::Normal) { Stop-ScriptWithError -ErrorMessage "Could not create service $service_name." }
+	Start-Service -Name "$service_name" | Out-Null
+	$start_status = (Get-Service -Name "$service_name").Status
+	Write-Log -Level DEBUG "Start status: $start_status"
+	if (!$start_status.Equals([System.ServiceProcess.ServiceControllerStatus]::Running)) { Stop-ScriptWithError -ErrorMessage "Could not start service $service_name." }
 
+	return Get-InstallationStatus $service_name
 }
-function Uninstall-Fsc
-{
-	param([bool]$FullUninstall, [String]$install_path)
 
-	$status = Get-InstallationStatus $install_path $false
-	if ($status -eq [ExistingInstallationStatus]::None) { return }
-	if ($status -eq [ExistingInstallationStatus]::Broken)
-	{
-		Write-Log -Level INFO -Message "The current installation of FSC is broken."
-	}
-	if ($FullUninstall -or ($status -eq [ExistingInstallationStatus]::Broken))
-	{
-		Write-Log -Level INFO -Message "Performing a full uninstallation of FSC."
-		if (-not $install_path) { $install_path = $InstallationKey.GetValue('InstallationPath') }
-		Unregister-ScheduledTask -TaskName "$ScheduledTaskName" -TaskPath "*\" -Confirm:$False -ErrorAction SilentlyContinue
-		Remove-Item -Recurse $install_path -Force -ErrorAction SilentlyContinue
-		Remove-Item -Recurse $InstallationKeyPath -Force -ErrorAction SilentlyContinue
-		return
-	}
-	if ($status -eq [ExistingInstallationStatus]::Normal)
-	{
-		if (-not $Force) { return }
-		$status = [ExistingInstallationStatus]::Update
-	}
-	if ($status -eq [ExistingInstallationStatus]::Update)
-	{
-		Write-Log -Level INFO -Message "Keeping current configuration and updating files."
-		trap { Remove-Item -Recurse $install_path -Force -ErrorAction SilentlyContinue }
-		return
-	}
-	Stop-ScriptWithError -ErrorMessage "Installation state is undefined."
+function Remove-Installation
+{
+	param([string]$service_name)
+	Stop-Service -Name "$service_name" -Force | Out-Null
+	Invoke-Expression -Command ("sc.exe " + "delete " + '"$service_name"') # there is no Remove-Service in Powershell 5.1 but thankfully this is a very simple command in sc
+	$status = (Get-InstallationStatus $service_name)
+	Write-Log -Level DEBUG "After deletion: $status"
+	if ($status -ne [ExistingInstallationStatus]::None) { Stop-ScriptWithError -ErrorMessage "Could not delete existing service $service_name." }
+	Write-Log -Level INFO -Message "Deleted existing service $service_name."
 }
